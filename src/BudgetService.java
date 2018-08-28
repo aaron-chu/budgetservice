3d034ac66ce0918171b6d238b2af17df1a1f5ab6
Aaron Chu <aaron.chu@linecorp.com>
2018-08-28 17:55:55 +0800

    :recycle: round value after sum

--- src/BudgetService.java
+++ src/BudgetService.java
@@ -20,13 +20,11 @@ public class BudgetService {
             List<Budget> collect = budgets.stream()
                     .filter(budget -> isSameYearMonth(budget, finalDate))
                     .collect(Collectors.toList());
-            if (collect.isEmpty()) {
-                budgetValue += 0.0;
-            } else {
+            if (!collect.isEmpty()) {
                 budgetValue += getBudgetPerDay(collect.get(0));
             }
         }
-        return budgetValue;
+        return Math.round(budgetValue * 100) / 100.0;
     }
 
     private boolean isSameYearMonth(Budget budget, LocalDate date) {
@@ -36,7 +34,7 @@ public class BudgetService {
 
     private double getBudgetPerDay(Budget budget) {
         LocalDate budgetLocalDate = convertBudgetYearMonthToLocalDate(budget);
-        return Math.round(budget.getAmount() * 1.0 / budgetLocalDate.lengthOfMonth() * 100) / 100.0;
+        return budget.getAmount() * 1.0 / budgetLocalDate.lengthOfMonth();
     }
 
     private LocalDate convertBudgetYearMonthToLocalDate(Budget budget) {
